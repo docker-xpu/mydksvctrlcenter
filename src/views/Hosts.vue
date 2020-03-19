61eff545fa26613f113a93eeb8ec04354e72107f
ahojcn <ahojcn@gmail.com>
2020-03-19 15:04:53 +0800

    feat 创建容器

--- src/views/Hosts.vue
+++ src/views/Hosts.vue
@@ -177,7 +177,7 @@
 
       <!--      创建容器 drawer-->
       <Drawer title="创建容器" width="50" :closable="true" v-model="showCreateContainerDrawer">
-        <Form v-model="createContainerForm" style="width: 500px">
+        <Form v-model="createContainerForm" style="width: 500px;">
           <FormItem>
             <label>容器名：
               <Input type="text" v-model="createContainerForm.container_name" placeholder="例如：mynginx1"></Input>
@@ -189,19 +189,67 @@
               <Input type="textarea" v-model="createContainerForm.cmd" placeholder="例如：nginx -g daemon off;"></Input>
             </label>
           </FormItem>
-          <FormItem label="容器卷：">
+          <FormItem
+                  v-for="(item, index) in createContainerForm.volumes"
+                  :key="index"
+                  v-if="item.status">
+            <Row>
+              <Col span="10">
+                <label>宿主机路径：
+                  <Input v-model="item.host_volume" placeholder="例如：/home/ahojcn"></Input>
+                </label>
+              </Col>
+              <Col span="10">
+                <label>容器内路径：
+                  <Input v-model="item.container_volume" placeholder="例如：/volume1"></Input>
+                </label>
+              </Col>
+              <Col span="4">
+                <Button icon="md-add" type="primary" ghost @click="handleAddVolume">增加</Button>
+                <Button type="error" ghost icon="md-arrow-round-back" @click="handleRemoveVolume(index)">删除</Button>
+              </Col>
+            </Row>
+          </FormItem>
+          <FormItem>
+            <label>工作目录：
+              <Input type="text" v-model="createContainerForm.working_dir" placeholder="例如：/root"></Input>
+            </label>
+          </FormItem>
+          <FormItem>
             <Row>
               <Col span="8">
-                <label>宿主机路径：
-                  <Input placeholder="例如：/home/ahojcn"></Input>
+                <label>网络协议：
+                  <Input type="text" v-model="createContainerForm.container_port_proto" placeholder="例如：tcp"></Input>
                 </label>
               </Col>
               <Col span="8">
+                <label>主机端口：
+                  <Input type="text" v-model="createContainerForm.host_port" placeholder="例如：8081"></Input>
+                </label>
               </Col>
-              <Col span="4">
-                <Button>增加</Button>
+              <Col span="8">
+                <label>容器端口：
+                  <Input type="text" v-model="createContainerForm.container_port" placeholder="例如：80"></Input>
+                </label>
               </Col>
             </Row>
+          </FormItem>
+          <FormItem>
+            <Row>
+              <Col span="12">
+                <label>CPU限制：
+                  <Input type="text" v-model="createContainerForm.cpu_shares" placeholder="例如：1024"></Input>
+                </label>
+              </Col>
+              <Col span="12">
+                <label>内存限制：
+                  <Input type="text" v-model="createContainerForm.memory" placeholder="例如：40000000"></Input>
+                </label>
+              </Col>
+            </Row>
+          </FormItem>
+          <FormItem>
+            <Button long type="primary" size="large" @click="createContainer">创建容器</Button>
           </FormItem>
         </Form>
       </Drawer>
@@ -271,8 +319,8 @@
           version: ""
         },
 
-        showContainerInfoDrawer: true,
-        showCreateContainerDrawer: true,
+        showContainerInfoDrawer: false,
+        showCreateContainerDrawer: false,
         showContainerInfo: {},
         containerInfoColumns: [
           {
@@ -321,7 +369,15 @@
           image_name: '',
           container_name: '',
           cmd: '',  // 提交时候需要处理
-          volumes: [],
+          volumes: [
+            {
+              host_volume: '',
+              container_volume: '',
+              index: 1,
+              status: 1
+            },
+          ],
+          volumesIndex: 1,
           working_dir: '',
           container_port_proto: '',
           container_port: '',
@@ -629,6 +685,52 @@
             })
           }
         });
+      },
+
+      // 创建容器当添加 volume
+      handleAddVolume() {
+        this.createContainerForm.volumesIndex++;
+        console.log(this.createContainerForm.volumes);
+        this.createContainerForm.volumes.push({
+          host_volume: '',
+          container_volume: '',
+          index: this.createContainerForm.volumesIndex,
+          status: 1
+        });
+      },
+      // 删除 volume
+      handleRemoveVolume(index) {
+        this.createContainerForm.volumes[index].status = 0;
+      },
+      // 创建容器
+      createContainer() {
+        let f = {};
+        f.ip = this.showContainerInfo.hostIp;
+        f.image_name = this.createContainerForm.image_name;
+        f.container_name = this.createContainerForm.container_name;
+        f.cmd = this.createContainerForm.cmd.split(' ');
+        f.working_dir = this.createContainerForm.working_dir;
+        f.container_port_proto = this.createContainerForm.container_port_proto;
+        f.container_port = this.createContainerForm.container_port;
+        f.host_port = this.createContainerForm.host_port;
+        f.cpu_shares = parseInt(this.createContainerForm.cpu_shares);
+        f.memory = parseInt(this.createContainerForm.memory);
+        f.volumes = [];
+        for (let i = 0; i < this.createContainerForm.volumes.length; ++i) {
+          f.volumes.push({
+            host_volume: this.createContainerForm.volumes[i].host_volume,
+            container_volume: this.createContainerForm.volumes[i].container_volume
+          })
+        }
+
+        createContainer(f).then(res => {
+          if (res.code === 0) {
+            this.$Message.success(res.msg);
+          } else {
+            this.$Message.error(res.msg);
+          }
+          this.$store.dispatch('getAllHost');
+        })
       },
     }
   };
